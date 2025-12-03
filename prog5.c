a73b2960e2052a6103e300f88c54baa55e7bb69e
MoNiMoNiSh18 <167692223+MoNiMoNiSh18@users.noreply.github.com>
2025-12-03 07:28:09 +0530

    Improve sliding window protocol in prog5.c
    Refactor sliding window protocol implementation for clarity and correctness.

--- prog5.c
+++ prog5.c
@@ -1,33 +1,34 @@
-#include<stdio.h>
-int main()
-{
-int i,f,w,frames[50];
-printf("Enter the window size:");
-scanf("%d",&w);
+#include <stdio.h>
 
-printf("Enter the number of frames:");
-scanf("%d",&f);
+int main() {
+    int i, f, w, frames[50];
 
-printf("Enter the frames ");
-for(i=1;i<=f;i++)
-scanf("%d",&frames[i]);
-printf("With Sliding Windows Protocol frames are received and an acknowledgement is sent to the sender");
-printf("The %d frames are sent to the receiver",w);
-for(i=1;i<=f;i++)
-{
-if(i%w==0)
-{
-printf("%d",frames[i]);
-printf("Acknowledgement for the above frames is received by the sender");
-}
-else
-{
-System.out.println("%d",frames[i]);
-}
-}
-if(i%w!=0)
-{
-printf("Acknowledgement for the above frames is received by the sender");
-}
-return 0;
+    printf("Enter window size: ");
+    scanf("%d", &w);
+
+    printf("Enter number of frames: ");
+    scanf("%d", &f);
+
+    printf("Enter frames: ");
+    for (i = 1; i <= f; i++) {
+        scanf("%d", &frames[i]);
+    }
+
+    printf("\nSliding Window Protocol Processing...\n");
+
+    printf("\nThe %d frames are sent to receiver\n", w);
+
+    for (i = 1; i <= f; i++) {
+        printf("%d ", frames[i]);
+
+        if (i % w == 0) {
+            printf("\nAcknowledgement received for above frames\n");
+        }
+    }
+
+    if ((i - 1) % w != 0) {
+        printf("\nAcknowledgement received for remaining frames\n");
+    }
+
+    return 0;
 }
